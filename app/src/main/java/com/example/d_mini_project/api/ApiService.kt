ed6289f691a81907996ecf04c1d8f08760753562
hanzeloliver <hanzel.oliver55@gmail.com>
2025-09-22 22:22:04 +0700

    Button udh bisa dan gambar udh ga ketabrak

--- app/src/main/java/com/example/d_mini_project/api/ApiService.kt
+++ app/src/main/java/com/example/d_mini_project/api/ApiService.kt
@@ -7,6 +7,6 @@ import retrofit2.http.Query
 
 // https://roxyapi.com/api/v1/data/astro/tarot/single-card-draw?token=b8409149-4d9c-4e2f-8d0d-1f14879294b2
 interface ApiService {
-    @GET("data/astro/tarot/single-card-draw?token=b8409149-4d9c-4e2f-8d0d-1f14879294b2")
+    @GET("data/astro/tarot/single-card-draw?token=89370406-b912-4705-b007-7e0a831116d6")
     fun getCardsData(): Call<CardsData>
 }
